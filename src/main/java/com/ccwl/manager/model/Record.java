39957600f246d5370a1bd3e8bccef2e7ac11044d
nancy0625 <1308013168@qq.com>
2019-10-28 10:28:32 +0800

    心惊胆战

---
+++ src/main/java/com/ccwl/manager/model/Record.java
@@ -0,0 +1,40 @@
+package com.ccwl.manager.model;
+
+public class Record {
+    String number;
+    String name;
+    String course;
+    String records;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getRecords() {
+        return records;
+    }
+
+    public void setRecords(String records) {
+        this.records = records;
+    }
+}
---
+++ src/main/java/com/ccwl/manager/service/RecordServiceImpl.java
@@ -0,0 +1,32 @@
+package com.ccwl.manager.service;
+
+import com.ccwl.manager.dao.ClassTableDao;
+import com.ccwl.manager.dao.RecordDao;
+import com.ccwl.manager.model.Course;
+import com.ccwl.manager.model.Record;
+import net.sf.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Service("recordServiceImpl")
+public class RecordServiceImpl {
+    @Autowired
+    HttpSession session;
+
+    @Resource(name = "recordDao")
+    private RecordDao recordDao;
+    public String RecordSelect(String number) {
+        List<Record> recordList = recordDao.getRecordByNumber(number);
+        if (recordList == null){
+            return "{\"state\": \"fail\", \"msg\": \"请检查账号是否正确！\"}";
+        }else{
+            this.session.setAttribute("USER", JSONArray.fromObject(recordList));
+            return "{\"state\": \"success\", \"msg\": " + JSONArray.fromObject( recordList) + "}";
+        }
+    }
+
+}
